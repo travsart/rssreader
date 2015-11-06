03654d532c1777ac86e5ac8b6b7c4c65374d4b55
travsart <travsart27@gmail.com>
2015-11-06 20:24:56 +0000

    yutt
    hh

---
+++ tasks/pipeline.js
@@ -0,0 +1,81 @@
+/**
+ * grunt/pipeline.js
+ *
+ * The order in which your css, javascript, and template files should be
+ * compiled and linked from your views and static HTML files.
+ *
+ * (Note that you can take advantage of Grunt-style wildcard/glob/splat expressions
+ * for matching multiple files.)
+ */
+
+
+
+// CSS files to inject in order
+//
+// (if you're using LESS with the built-in default config, you'll want
+//  to change `assets/styles/importer.less` instead.)
+var cssFilesToInject = [
+  'styles/dependencies/bootstrap.css',
+  'styles/dependencies/ui-grid.min.css',
+  'styles/dependencies/toaster.min.css',
+  'styles/dependencies/dialogs.min.css',
+  'styles/**/*.css'
+];
+
+
+// Client-side javascript files to inject in order
+// (uses Grunt-style wildcard/glob/splat expressions)
+var jsFilesToInject = [
+  
+  // Load sails.io before everything else
+  'js/dependencies/sails.io.js',
+  'js/dependencies/angular.min.js',
+  'js/dependencies/ngsails.io.js',
+  'js/dependencies/ui-grid.js',
+  'js/dependencies/angular-sails.min.js',
+  'js/dependencies/ui-bootstrap-tpls.min.js',
+  'js/dependencies/angular-animate.min.js',
+  'js/dependencies/angular-sanitize.min.js',
+  'js/dependencies/angular-translate.min.js',
+  'js/dependencies/dialogs-default-translations.min.js',
+  'js/dependencies/dialogs.min.js',
+  'js/dependencies/toaster.min.js',
+
+  // Dependencies like jQuery, or Angular are brought in here
+  'js/app/app.js',
+  'js/app/controller/*.js',
+  'js/app/service/*.js',
+
+  // All of the rest of your client-side js files
+  // will be injected here in no particular order.
+  'js/**/*.js'
+];
+
+
+// Client-side HTML templates are injected using the sources below
+// The ordering of these templates shouldn't matter.
+// (uses Grunt-style wildcard/glob/splat expressions)
+//
+// By default, Sails uses JST templates and precompiles them into
+// functions for you.  If you want to use jade, handlebars, dust, etc.,
+// with the linker, no problem-- you'll just want to make sure the precompiled
+// templates get spit out to the same file.  Be sure and check out `tasks/README.md`
+// for information on customizing and installing new tasks.
+var templateFilesToInject = [
+  'templates/**/*.html'
+];
+
+
+
+// Prefix relative paths to source files so they point to the proper locations
+// (i.e. where the other Grunt tasks spit them out, or in some cases, where
+// they reside in the first place)
+module.exports.cssFilesToInject = cssFilesToInject.map(function(path) {
+  return '.tmp/public/' + path;
+});
+module.exports.jsFilesToInject = jsFilesToInject.map(function(path) {
+  return '.tmp/public/' + path;
+});
+module.exports.templateFilesToInject = templateFilesToInject.map(function(path) {
+  return 'assets/' + path;
+});
