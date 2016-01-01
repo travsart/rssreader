9b9295c7b553ac4a0b1c72fb3a9f5469cac09514
travsart <travsart27@gmail.com>
2016-01-01 15:03:55 +0000

    as

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports.auth = {
+    bcrypt: {
+        /**
+         * Specifiy number of salt rounds to perform on password. Values >10 are
+         * slow.
+         */
+        rounds: 8
+    }
+}
