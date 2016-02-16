21f586bdac11664accc9ce214a6115da1536be7a
travsart <travsart27@gmail.com>
2016-02-16 21:30:52 +0000

    as

--- parse.js
+++ parse.js
@@ -16,8 +16,6 @@ function parseManga (body, cb) {
     };
     chBody.find('div.content').each(function (index, content) {
         console.log(content);
-        content = cheerio(content);
-
         console.log(content.children[1]);
         console.log(cheerio(content.children[1]).children[0]);
         console.log(cheerio(content.children[1]).children[0]);
