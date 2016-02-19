07eb2f7e4e6062cedc75a2b390e34387e764bc91
server <server@localhost.localdomain>
2016-02-19 22:11:56 +0000

    s

---
+++ .tmp/public/js/app/app.js
@@ -0,0 +1,3 @@
+var rssApp = angular.module("rss", ['ui.grid', 'ui.grid.edit',
+    'ui.bootstrap', 'ui.grid.resizeColumns', 'toaster', 'dialogs.main',
+    'pascalprecht.translate', 'dialogs.default-translations', 'ui.grid.moveColumns']);
