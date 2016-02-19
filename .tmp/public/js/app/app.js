adeb0b7cf04eff19ad3071ec5c0546970d6a0c44
server <server@localhost.localdomain>
2016-02-19 22:12:35 +0000

    s

--- .tmp/public/js/app/app.js
+++
@@ -1,3 +0,0 @@
-var rssApp = angular.module("rss", ['ui.grid', 'ui.grid.edit',
-    'ui.bootstrap', 'ui.grid.resizeColumns', 'toaster', 'dialogs.main',
-    'pascalprecht.translate', 'dialogs.default-translations', 'ui.grid.moveColumns']);
--- .tmp/public/js/app/service/loginService.js
+++
@@ -1,9 +0,0 @@
-rssApp.factory('loginService', function ($http) {
-    var url = '/login';
-
-    return {
-        login: function (data) {
-            return $http.post(url, data);
-        }
-    };
-});
