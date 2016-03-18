fe8d603761bf84f68986f340d3c404c7e453dfde
travsart <travsart27@gmail.com>
2016-03-18 16:37:53 +0000

    as

--- api/services/GenreService.js
+++ api/services/GenreService.js
@@ -20,7 +20,8 @@ module.exports = {
             content = cheerio(content);
 
             var innerTable = cheerio(cheerio(content.children[1]).find('table .attr'));
-            sails.log.debug(innerTable);
+            sails.log.debug(content.children[1]);
+            sails.log.debug(content.children[1].children);
             innerTable.find('tr').each(function (row) {
                 sails.log.info(row.html());
                 sails.log.info(row.children[0]);
