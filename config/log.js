cd4cf43c70b11a82e3edfa32040f2d125cdf77b2
travsart <travsart27@gmail.com>
2016-04-17 20:32:32 +0100

    as

--- config/log.js
+++ config/log.js
@@ -12,47 +12,47 @@
 var winston = require('winston');
 process.env.LOG_PATH = require('path').resolve('.');
 
-
+winston.remove(winston.transports.Console);
 module.exports.log = {
     transports: [{
-		module: winston.transports.DailyRotateFile,
-		config:{
-			dirname: process.env.LOG_PATH,
-			name: 'dr.debug',
-			datePattern: '_yyyy-MM-dd.log',
-			filename: 'log_debug',
-			timestamp: true,
-			level: 'debug',
-			json: false,
-			colorize:false,
-			zippedArchive: true
-		}
-	},
-	{
-		module: winston.transports.DailyRotateFile,
-		config:{
-			dirname: process.env.LOG_PATH,
-			name: 'dr.info',
-			datePattern: '_yyyy-MM-dd.log',
-			filename: 'log_info',
-			timestamp: true,
-			level: 'info',
-			json: false,
-			colorize:false,
-			zippedArchive: true
-		}
-    }, {
-		module: winston.transports.DailyRotateFile,
-		config:{
-			dirname: process.env.LOG_PATH,
-			name: 'dr.warn',
-			datePattern: '_yyyy-MM-dd.log',
-			filename: 'log_warn',
-			timestamp: true,
-			level: 'warn',
-			json: false,
-			colorize:false,
-			zippedArchive: true
-		}
-    }]
+        module: winston.transports.DailyRotateFile,
+        config: {
+            dirname: process.env.LOG_PATH,
+            name: 'dr.debug',
+            datePattern: '_yyyy-MM-dd.log',
+            filename: 'log_debug',
+            timestamp: true,
+            level: 'debug',
+            json: false,
+            colorize: false,
+            zippedArchive: true
+        }
+    },
+        {
+            module: winston.transports.DailyRotateFile,
+            config: {
+                dirname: process.env.LOG_PATH,
+                name: 'dr.info',
+                datePattern: '_yyyy-MM-dd.log',
+                filename: 'log_info',
+                timestamp: true,
+                level: 'info',
+                json: false,
+                colorize: false,
+                zippedArchive: true
+            }
+        }, {
+            module: winston.transports.DailyRotateFile,
+            config: {
+                dirname: process.env.LOG_PATH,
+                name: 'dr.warn',
+                datePattern: '_yyyy-MM-dd.log',
+                filename: 'log_warn',
+                timestamp: true,
+                level: 'warn',
+                json: false,
+                colorize: false,
+                zippedArchive: true
+            }
+        }]
 };
